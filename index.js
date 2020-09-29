30ed024198f35c16f3430a4527168a2b66686927
Oleksandr <bilovka10@gmail.com>
2020-09-29 18:09:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr=[];
+  for(var i=0;i<musicians.length;i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+var a=0;
+var arr=[];
+while(a<facts.length){
+  arr.push(`${facts[a]}!!!`);
+  a++;
+}
+return arr;
+}
+
+function iLoveTheBeatles(num){
+  var arr=[];
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  }
+  while(num<15);
+  return arr;
+}
